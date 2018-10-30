41373858a5e48f7be8521b3aced1727cd0efa05e
Agustiawan <agustiawan@student.undip.ac.id>
2018-10-30 20:21:23 +0700

    Modul 1 Kelompok 37

---
+++ Kelompok37/Kelompok37/Model/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kelompok37.Model
+{
+    public static class UserLogin
+    {
+        public static string Username = "Kelompok37";
+        public static string Password = "password";
+    }
+}
---
+++ Kelompok37/Kelompok37/View/HalamanHapusData.cs
@@ -0,0 +1,55 @@
+﻿using Kelompok37.Model;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Kelompok37.View
+{
+    public class HalamanHapusData : ContentPage
+    {
+        private ListView _listView;
+        private Button _hapus;
+
+        DataMahasiswa dataMahasiswa = new DataMahasiswa();
+
+        string _dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "myDB.db4");
+
+        public HalamanHapusData()
+        {
+            this.Title = "Hapus Data";
+
+            var db = new SQLiteConnection(_dbPath);
+
+            StackLayout stackLayout = new StackLayout();
+
+            _listView = new ListView();
+            _listView.ItemsSource = db.Table<DataMahasiswa>().OrderBy(x => x.Nama).ToList();
+            _listView.ItemSelected += _listView_ItemSelected;
+            stackLayout.Children.Add(_listView);
+
+            _hapus = new Button();
+            _hapus.Text = "Hapus";
+            _hapus.Clicked += _hapus_Clicked;
+            stackLayout.Children.Add(_hapus);
+
+            Content = stackLayout;
+        }
+
+        private async void _hapus_Clicked(object sender, EventArgs e)
+        {
+            var db = new SQLiteConnection(_dbPath);
+            db.Table<DataMahasiswa>().Delete(x => x.Nama == dataMahasiswa.Nama);
+            await Navigation.PopAsync();
+        }
+
+        private void _listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            dataMahasiswa = (DataMahasiswa)e.SelectedItem;
+        }
+    }
+}
